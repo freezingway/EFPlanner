af81a9714435c0e19f53de227b5cfda36cb4a547
freezingway <mwegner11093@gmail.com>
2026-03-07 19:48:12 +0100

    repo init

---
+++ main.py
@@ -0,0 +1,8 @@
+from PySide6.QtWidgets import QApplication, QWidget
+import sys
+
+app = QApplication([])
+window = QWidget()
+window.show()
+
+app.exec()
---
+++ readme.md
@@ -0,0 +1,3 @@
+# EFPlanner
+
+Simpe tool to plan character and weapon upgrades for Arknights: Endfield
